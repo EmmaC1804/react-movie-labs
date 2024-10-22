b8b23c8f6047d1c092528148f3f08292b85484e3
EmmaC1804 <114242779+EmmaC1804@users.noreply.github.com>
2024-10-22 12:19:12 +0100

    Refactor: Favourite movie list now stored in a context

--- movies/src/components/movieCard/index.js
+++ movies/src/components/movieCard/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useContext  } from "react";
+import { MoviesContext } from "../../contexts/moviesContext";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -15,13 +16,20 @@ import img from '../../images/film-poster-placeholder.png'
 import { Link } from "react-router-dom";
 import { Avatar } from "@mui/material";
 
-export default function MovieCard(props) {
-  const movie = props.movie;
+export default function MovieCard({movie}) {
 
-const handleAddToFavorite = (e) => {
-  e.preventDefault();
-  props.selectFavorite(movie.id);
-};
+  const { favorites, addToFavorites } = useContext(MoviesContext);
+
+  if (favorites.find((id) => id === movie.id)) {
+    movie.favorite = true;
+  } else {
+    movie.favorite = false
+  }
+
+  const handleAddToFavorite = (e) => {
+    e.preventDefault();
+    addToFavorites(movie);
+  };
 
   return (
     <Card>
